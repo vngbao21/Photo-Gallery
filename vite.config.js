78db6877a4a2f6640395e7baa5348348d9f892fa
Gia Bẻo <baog3083@gmail.com>
2024-10-25 22:32:16 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Cho phép truy cập từ mọi địa chỉ
+    port: 5173,        // Đảm bảo cổng không bị thay đổi
+    open: true,        // Mở trình duyệt sau khi khởi chạy
+  },
+});
